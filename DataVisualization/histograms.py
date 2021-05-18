343413bde99a71ecc13f6f73b4e8fd1af16f8b9c
Spyro1322 <44666695+Spyro1322@users.noreply.github.com>
2021-05-18 17:15:10 +0300

    To be continued...

--- DataVisualization/histograms.py
+++ DataVisualization/histograms.py
@@ -8,18 +8,17 @@ games_details = pd.read_csv('../Data/games_details.csv')
 games = pd.read_csv('../Data/games.csv')
 teams = pd.read_csv('../Data/teams.csv')
 
-def hist_plot(df, category):
+def hist_plot(df, *category):
     # Plotting function for histograms per given stat category
 
     sns.set_palette("rocket")
+    for cats in category:
+        plt.hist(df[cats], bins=int(np.sqrt(len(df[cats]))))
+        plt.xlabel("Number of %s" %(cats))
+        plt.ylabel("Number of games")
+        plt.title("Number of %s made" %(cats))
+        plt.show()
+        mean_h = np.mean(df[cats])
+        std_h=np.std(df[cats])
 
-    plt.hist(df[category], bins=int(np.sqrt(len(df[category]))))
-    plt.xlabel("Number of %s" %(category))
-    plt.ylabel("Number of games")
-    plt.show()
-    mean_h = np.mean(df[category])
-    std_h=np.std(df[category])
-
-    print("mean:", mean_h, "std:", std_h)
-
-hist_plot(games, "PTS_home")
+        print("mean:", mean_h, "std:", std_h)
