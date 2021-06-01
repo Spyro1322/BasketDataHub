89d917c1defdb7fa4cd5025f30df5a4ea1098bf4
Spyro1322 <44666695+Spyro1322@users.noreply.github.com>
2021-06-01 12:36:49 +0300

    Add files via upload

---
+++ venv/DataVisualization/BasicSteps.py
@@ -0,0 +1,40 @@
+from utilities import *
+
+# Dataframes - dfs
+games_details = pd.read_csv('../Data/games_details.csv')
+games = pd.read_csv('../Data/games.csv')
+teams = pd.read_csv('../Data/teams.csv')
+
+# Delete unnecessary columns
+games_details.drop(['GAME_ID', 'TEAM_ID', 'PLAYER_ID', 'START_POSITION', 'COMMENT', 'TEAM_ABBREVIATION'], axis=1, inplace=True)
+games_details = games_details.dropna()
+
+
+# plt.figure(figsize=(14, 7))
+# sns.heatmap(games_details.corr(), cmap='coolwarm', annot=True)
+# sns.heatmap(games.corr(), cmap='coolwarm', annot=True)
+# sns.heatmap(teams.corr(), cmap='coolwarm', annot=True)
+
+
+df_tmp = games_details[['PLAYER_NAME', 'MIN']]
+df_tmp.loc[:, 'MIN'] = df_tmp['MIN'].apply(convert_min)
+agg = df_tmp.groupby('PLAYER_NAME').agg('sum').reset_index()
+agg.columns = ['PLAYER_NAME', 'Number of seconds played']
+
+# Maybe process some Team overall stats as wins,losses etc.
+winning_teams = np.where(games['HOME_TEAM_WINS'] == 1, games['HOME_TEAM_ID'], games['VISITOR_TEAM_ID'])
+winning_teams = pd.DataFrame(winning_teams, columns=['TEAM_ID'])
+winning_teams = winning_teams.merge(teams[['TEAM_ID', 'NICKNAME']], on='TEAM_ID')['NICKNAME'].value_counts().to_frame().reset_index()
+winning_teams.columns = ['TEAM NAME', 'Number of wins']
+
+
+dataset_overview(games_details, 'games_details')
+dataset_overview(games, 'games')
+blind_plot(agg, column='Number of seconds played', label_col='PLAYER_NAME', max_plot=10)
+blind_plot(winning_teams, column='Number of wins', label_col='TEAM NAME', max_plot=10)
+column_distribution(games_details, 10, 5)
+correlation_matrix(games_details, 8)
+correlation_matrix(games, 8)
+scatter_matrix(games_details, 20, 10)
+
+
---
+++ venv/DataVisualization/boxes.py
@@ -0,0 +1,56 @@
+from utilities import *
+import matplotlib.pyplot as plt
+import seaborn as sns
+import click
+
+
+# Check '18 - '19 different statistical categories for the teams
+# Dataframes - dfs, games.csv is interesting enough in combination with the others that have already been used
+games_details = pd.read_csv('../Data/games_details.csv')
+games = pd.read_csv('../Data/games.csv')
+teams = pd.read_csv('../Data/teams.csv')
+
+games = games.dropna()
+
+# Select Team-Abbreviation for easier coding
+seasons = [2018, 2019]
+games_est = games[games["SEASON"].isin(seasons)]
+win = games_est["HOME_TEAM_WINS"]
+
+# Delete unnecessary columns
+games_est = games_est.drop(columns=["TEAM_ID_home", "TEAM_ID_away", "GAME_STATUS_TEXT"])
+
+# Select Team-Abbreviation for easier coding
+trans = teams.set_index("TEAM_ID")["ABBREVIATION"].to_dict()
+# print(trans)
+games_est["HOME_TEAM_ID"] = games_est["HOME_TEAM_ID"].replace(trans)
+games_est["VISITOR_TEAM_ID"] = games_est["VISITOR_TEAM_ID"].replace(trans)
+
+@click.command()
+@click.argument('category', type=str, nargs=-1)
+# Plot season stats for Teams
+def home_overall_stats(category):
+    # Choose home teams' stats.
+    for cat in category:
+        sns.boxplot(x="HOME_TEAM_ID", y=cat, data=games_est)
+        plt.xlabel("HOME TEAM", size=12)
+        plt.xticks(rotation=90)
+        plt.ylabel("%s MADE" % cat, size=12)
+        plt.title("Home Teams' Numbers in %s for 18-19 Season" % cat, size=10)
+        plt.tight_layout()
+        plt.show()
+
+@click.command()
+@click.argument('category', type=str, nargs=1)
+def away_overall_stats(category):
+    for cate in category:
+        sns.boxplot(x="VISITOR_TEAM_ID", y=cate, data=games_est)
+        plt.xlabel("VISITOR TEAM")
+        plt.xticks(rotation=90)
+        plt.ylabel("%s MADE" % cate)
+        plt.title("AWAY Teams' Numbers in %s for 18-19 Season" % cate)
+        plt.tight_layout()
+        plt.show()
+
+if __name__ == '__main__':
+    home_overall_stats()
