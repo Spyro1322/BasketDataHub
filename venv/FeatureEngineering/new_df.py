037fe1a8f30a6d24b7799336e883b3cd7bd20d35
Spyro1322 <44666695+Spyro1322@users.noreply.github.com>
2021-11-11 12:59:50 +0200

    There are improvements to be made...

--- venv/FeatureEngineering/new_df.py
+++ venv/FeatureEngineering/new_df.py
@@ -63,44 +63,5 @@ input_data['MISSING_PLAYER_DIFF'] = input_data.MISSING_PLAYERS - input_data.MISS
 input_data['EFF_DIFF'] = input_data.EFF - input_data.EFF_VISITOR
 input_data['MONTH'] = input_data.GAME_DATE_EST.dt.month
 
-# input_data.drop(columns=['CONFERENCE', 'CONFERENCE_VISITOR'],
-#                 inplace=True)
-
 # print(input_data)
-input_data.to_csv('../Data/input_data1.csv')
-
-
-# Given a team and a date, this method will return that teams average stats over the previous n games
-# def get_avg_stats_last_n_games(team, game_date, season_team_stats, n):
-#     prev_game_df = season_team_stats[season_team_stats['Date'] < game_date][
-#         (season_team_stats['H_Team'] == team) | (season_team_stats['A_Team'] == team)].sort_values(by='Date').tail(n)
-#
-#     h_df = prev_game_df.iloc[:, range(3, 43, 2)]
-#     h_df.columns = [x[2:] for x in h_df.columns]
-#     a_df = prev_game_df.iloc[:, range(4, 44, 2)]
-#     a_df.columns = [x[2:] for x in a_df.columns]
-#
-#     df = pd.concat([h_df, a_df])
-#     df = df[df['Team'] == team]
-#     df.drop(columns=['Team'], inplace=True)
-#
-#     return df.mean()
-
-# train_data = input_data.loc[(input_data.SEASON < 2018) & (input_data.SEASON > 2005)]
-# valid_data = input_data.loc[input_data.SEASON == 2018]
-# test_data = input_data.loc[input_data.SEASON == 2019]
-# full_train_data = pd.concat([train_data, valid_data], axis=0)
-#
-# X, y = train_data.drop(columns=['HOME_TEAM_WINS']), train_data.HOME_TEAM_WINS
-# valid_X, valid_y = valid_data.drop(columns=['HOME_TEAM_WINS']), valid_data.HOME_TEAM_WINS
-# test_X, test_y = test_data.drop(columns=['HOME_TEAM_WINS']), test_data.HOME_TEAM_WINS
-# full_train_X, full_train_y = full_train_data.drop(
-#     columns=['HOME_TEAM_WINS', 'SEASON', 'GAME_ID']), full_train_data.HOME_TEAM_WINS
-#
-# train_games = X[['SEASON', 'GAME_ID']]
-# valid_games = valid_X[['SEASON', 'GAME_ID']]
-# test_games = test_X[['SEASON', 'GAME_ID']]
-#
-# X.drop(columns=['SEASON', 'GAME_ID'], inplace=True)
-# valid_X.drop(columns=['SEASON', 'GAME_ID'], inplace=True)
-# test_X.drop(columns=['SEASON', 'GAME_ID'], inplace=True)
+# input_data.to_csv('../Data/input_data1.csv')
